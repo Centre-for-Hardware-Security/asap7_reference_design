f056a6724f9c04438b409cb3b24ca3b00809a9fe
Samuel Pagliarini <50336652+snpagliarini@users.noreply.github.com>
2022-07-27 15:53:41 +0300

    change genus script to reflect directory structure

--- scripts/genus.tcl
+++ scripts/genus.tcl
@@ -1,12 +1,13 @@
-#script written by S. Pagliarini on July 2022.
-# to modify it, look for TODO markers
+#script written by S. Pagliarini on July 2022. works well in genus 18.10
+
+# to modify this script, look for TODO markers
 
 #TODO change this RTL path to point to the folder containing your design
-set RTL_PATH		"../run/"
+set RTL_PATH		"../sources/"
 set LIB_PATH 		"../lib/"
-set LEF_PATH		"../lef/"
+set LEF_PATH		"../lef/scaled/"
 set TLEF_PATH		"../techlef/"
-set QRC_PATH		"../qrc"
+set QRC_PATH		"../qrc/"
 
 
 # TODO change my name
@@ -26,7 +27,7 @@ set DESIGN 		"sha256"
 set LIB_LIST {  asap7sc7p5t_AO_LVT_TT_nldm_211120.lib   asap7sc7p5t_INVBUF_LVT_TT_nldm_220122.lib   asap7sc7p5t_OA_LVT_TT_nldm_211120.lib   asap7sc7p5t_SEQ_LVT_TT_nldm_220123.lib   asap7sc7p5t_SIMPLE_LVT_TT_nldm_211120.lib \
  		asap7sc7p5t_AO_SLVT_TT_nldm_211120.lib  asap7sc7p5t_INVBUF_SLVT_TT_nldm_220122.lib  asap7sc7p5t_OA_SLVT_TT_nldm_211120.lib  asap7sc7p5t_SEQ_SLVT_TT_nldm_220123.lib  asap7sc7p5t_SIMPLE_SLVT_TT_nldm_211120.lib}
 
-set LEF_LIST { asap7_tech_4x_201209.lef asap7sc7p5t_28_L_1x_220121a.lef}
+set LEF_LIST { asap7_tech_4x_201209.lef asap7sc7p5t_28_L_4x_220121a.lef asap7sc7p5t_28_R_4x_220121a.lef asap7sc7p5t_28_SL_4x_220121a.lef}
 
 # All HDL files, separated by spaces
 set RTL_LIST {sha256.v sha256_core.v sha256_k_constants.v sha256_w_mem.v  }
@@ -45,9 +46,9 @@ elaborate $DESIGN
 # Read the constraint file
 #TODO these are very simple constraints, you should probably use an SDC file instead
 # the library uses picoseconds as time unit. this causes confusion because default unit in genus is ns
-create_clock -name "clk" -period 1000 [get_ports clk]
+create_clock -name "clk" -period 600 [get_ports clk]
 set_input_delay -clock clk 1 [all_inputs]
-set_output_delay -clock clk 500 [all_outputs]
+set_output_delay -clock clk 300 [all_outputs]
 
 # GENERIC SYNTHESIS
 syn_generic
@@ -58,10 +59,14 @@ syn_map
 # OPT
 syn_opt
 
+#TODO this will overwrite any previous netlist you might have. comment out if you don't want this behavior
+write_hdl > netlist.v
+
+#TODO uncomment these lines to get reports directly on text files
 # REPORTING (Timing, Area, Gates, Power)
-#report timing > ../report/genus_timing.rep
-#report area	  > ../report/genus_area.rep
-#report gates  > ../report/genus_cell.rep
-#report power  > ../report/genus_power.rep
+#report timing > ./genus_timing.rep
+#report area   > ./genus_area.rep
+#report gates  > ./genus_cell.rep
+#report power  > ./genus_power.rep
 
 
